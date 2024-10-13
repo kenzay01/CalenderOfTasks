2e7e2ed646824454715e839e34062da45f6dbd0f
kenzay <imdanya946@gmail.com>
2024-10-13 22:47:18 +0300

    added type User

---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  username: string;
+  fullname?: string;
+  age?: number;
+  occupation?: string;
+  email?: string;
+  phone?: string;
+  telegram?: string;
+  imgUrl?: string;
+}
